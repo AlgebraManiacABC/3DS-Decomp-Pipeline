f903b63363fc6981da1dd52793f66d69ceb08c85
AlgebraManiacABC <dunnluke01@gmail.com>
2026-03-02 03:29:17 -0600

    Improve ELF parsing

--- ELF.py
+++ ELF.py
@@ -8,23 +8,32 @@ class ELFHeader:
     SHENTSIZE = 0x28
     FLAGS = 0x05000000
 
-    def __init__(self, shoff: int, shnum: int, shstrndx: int):
+    def __init__(self, shoff: int, shnum: int, shstrndx: int, valid: bool):
         self.shoff = shoff
         self.shnum = shnum
         self.shstrndx = shstrndx
+        self.valid = valid
         return
 
     @classmethod
     def from_reader(cls, reader: BinaryReader) -> "ELFHeader":
+        reader.seek(0)
+        check = reader.read_bytes(4)
+        if check != b'\x7fELF':
+            return cls(0, 0, 0, False)
         reader.seek(0x20)
         shoff = reader.read_u32()
         reader.seek(0x30)
         shnum = reader.read_u16()
         shstrndx = reader.read_u16()
-        return cls(shoff, shnum, shstrndx)
+        return cls(shoff, shnum, shstrndx, True)
 
-    def write(self, path: Path):
+    def write_standalone(self, path: Path):
         writer = BinaryWriter()
+        self.write(writer)
+        writer.flush(path)
+
+    def write(self, writer: BinaryWriter):
         writer.write_bytes(self.IDENT)
         writer.write_bytes(self.T_M_V)
         writer.write_u32(0)  # e_entry
@@ -37,13 +46,12 @@ class ELFHeader:
         writer.write_u16(self.SHENTSIZE)
         writer.write_u16(self.shnum)
         writer.write_u16(self.shstrndx)
-        writer.flush(path)
 
 
 class SectionHeaderEntry:
     def __init__(self, name_off: int, type: int, flags: int, addr: int, off: int,
                  size: int, link: int = 0, info: int = 0,
-                 addralign: int = 0x4, entsize: int = 0):
+                 entsize: int = 0, addralign: int = 0x4):
         self.name_off = name_off
         self.type = type
         self.flags = flags
@@ -52,8 +60,8 @@ class SectionHeaderEntry:
         self.size = size
         self.link = link
         self.info = info
-        self.addralign = addralign
         self.entsize = entsize
+        self.addralign = addralign
 
     @classmethod
     def from_reader(cls, reader: BinaryReader) -> "SectionHeaderEntry":
@@ -67,7 +75,7 @@ class SectionHeaderEntry:
         info = reader.read_u32()
         addralign = reader.read_u32()
         entsize = reader.read_u32()
-        return cls(name_off, type, flags, addr, off, size, link, info, addralign, entsize)
+        return cls(name_off, type, flags, addr, off, size, link, info, entsize, addralign)
 
     def write(self, writer: BinaryWriter):
         writer.write_u32(self.name_off)
@@ -118,19 +126,41 @@ class SymbolTableEntry:
         return f"name offset {self.name_off:08x} ({self.size} bytes) [wip] -> section header #{self.shndx}"
 
 
+class SectionHeaderType(IntEnum):
+    SHT_NULL = 0
+    SHT_PROGBITS = 1
+    SHT_SYMTAB = 2
+    SHT_STRTAB = 3
+    SHT_RELA = 4
+    SHT_HASH = 5
+    SHT_DYNAMIC = 6
+    SHT_NOTE = 7
+    SHT_NOBITS = 8
+    SHT_REL = 9
+    SHT_SHLIB = 10
+    SHT_DYNSYM = 11
+
+
 class ELF:
-    def __init__(self, header: ELFHeader, data: bytes, mask: Bitmask, imported: list[str]):
+    def __init__(self, header: ELFHeader, data: bytes, data_off: int, mask: Bitmask,
+                 imported: list[str], strtab_bytes: bytes, symtab_entries: list[SymbolTableEntry]):
         self.header = header
         self.data = data
+        self.data_off = data_off
         self.mask = mask
         self.imported_symbols = imported
+        self.strtab_bytes = strtab_bytes
+        self.symtab_entries = symtab_entries
         return
 
     @classmethod
     def from_reader(cls, reader: BinaryReader) -> "ELF":
         header = ELFHeader.from_reader(reader)
+        if not header.valid:
+            return cls(header, b'\x00', 0, Bitmask(0), [], b'\x00', [])
         sh_entries = []
         text_data = []
+        data_off = 0
         text_name_offsets = []
         symtab_entries = []
         strtab_index = 0
@@ -146,13 +176,15 @@ class ELF:
                     reader.seek(sh_entry.off)
                     text_data.append(reader.read_bytes(sh_entry.size))
                     text_name_offsets.append(sh_entry.name_off)
+                    data_off = sh_entry.addr
                 case 2:
                     # .symtab
                     strtab_index = sh_entry.link
                     reader.seek(sh_entry.off)
                     num_symbols = int(sh_entry.size / 0x10)
                     for j in range(num_symbols):
-                        symtab_entries.append(SymbolTableEntry.from_reader(reader))
+                        sym = SymbolTableEntry.from_reader(reader)
+                        symtab_entries.append(sym)
                 case 9:
                     # .rel.xyz (e.g., .rel.debug, .rel.text)
                     rel_indices.append(i)
@@ -197,8 +229,81 @@ class ELF:
                     undefined_symbols.append(rel_name)
                     mask.add_relocation(rel_entry)
 
-        return cls(header, bin_bytes, mask, undefined_symbols)
+        return cls(header, bin_bytes, data_off, mask, undefined_symbols, strings, symtab_entries)
 
     @classmethod
     def from_path(cls, path: Path) -> "ELF":
         return cls.from_reader(BinaryReader.from_path(path))
+
+    @classmethod
+    def from_bytes(cls, b: bytes, data_off: int, to_export: list[str], sym_list: list[Symbol], name: str) -> "ELF":
+        header = ELFHeader(0, 0, 0, True)
+        mask = Bitmask(len(b))
+        strtab_bytes = bytearray(b'\x00')
+        symtab_entries = []
+        for sym in to_export.copy():
+            # match with sym_list
+            addr = -1
+            for s in sym_list:
+                if sym == s.name:
+                    addr = s.addr
+                    break
+            if addr < 0:
+                continue
+            # NOTE: st_other == 0x2 ("hidden"); decomp.me creates hidden exports by default
+            symtab_entries.append(SymbolTableEntry(len(strtab_bytes),
+                addr, len(sym) + 1, 0x12, 0x2, 1))
+            strtab_bytes += sym.encode('utf-8') + b'\x00'
+            to_export.remove(sym)
+        elf = cls(header, b, data_off, mask, [], strtab_bytes, symtab_entries)
+        return elf
+
+    def write(self, o_file: Path):
+        writer = BinaryWriter()
+        self.header.write(writer)
+        # .text
+        text_off = writer.tell()
+        writer.write_bytes(self.data)
+        # .symtab
+        symtab_off = writer.tell()
+        writer.write_bytes(b'\x00' * 0x10) # 0th entry is null
+        for st_entry in self.symtab_entries:
+            st_entry.write(writer)
+        # .strtab
+        strtab_off = writer.tell()
+        writer.write_bytes(self.strtab_bytes)
+        # .shstrtab
+        shstrtab_off = writer.tell()
+        writer.write_u8(0)
+        text_name_off = writer.tell() - shstrtab_off
+        writer.write_str('.text')
+        symtab_name_off = writer.tell() - shstrtab_off
+        if self.symtab_entries:
+            writer.write_str('.symtab')
+        strtab_name_off = writer.tell() - shstrtab_off
+        if self.symtab_entries:
+            writer.write_str('.strtab')
+        shstrtab_name_off = writer.tell() - shstrtab_off
+        writer.write_str('.shstrtab')
+        # Section header entries
+        sh_off = writer.tell()
+        SectionHeaderEntry(0, 0, 0, 0, 0,
+                           0, 0, 0, 0, 0).write(writer)
+        SectionHeaderEntry(text_name_off, SectionHeaderType.SHT_PROGBITS, 0x6, self.data_off, text_off,
+                           symtab_off - text_off, 0, 0, 0).write(writer)
+        if self.symtab_entries:
+            SectionHeaderEntry(symtab_name_off, SectionHeaderType.SHT_SYMTAB, 0, 0, symtab_off,
+                           strtab_off - symtab_off, 3, 1, 0x10).write(writer)
+            SectionHeaderEntry(strtab_name_off, SectionHeaderType.SHT_STRTAB, 0, 0, strtab_off,
+                           len(self.strtab_bytes), 0, 0, 0).write(writer)
+        SectionHeaderEntry(shstrtab_name_off, SectionHeaderType.SHT_STRTAB, 0, 0, shstrtab_off,
+                           sh_off - shstrtab_off, 0, 0, 0, 0).write(writer)
+
+        # Fix header
+        writer.seek(0x20)
+        writer.write_u32(sh_off)
+        writer.seek(0x30)
+        writer.write_u16(5 if self.symtab_entries else 3) # shnum
+        writer.write_u16(4 if self.symtab_entries else 2) # shstrndx
+
+        writer.flush(o_file)
